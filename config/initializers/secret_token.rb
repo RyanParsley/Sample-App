32ff462bb186d2792d34a1c0dc4cc5b87c6378bc
Ryan Parsley <RMParsley@gmail.com>
2012-02-27 22:02:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourGoodHours::Application.config.secret_token = '145df8247fc22a153140463d22c1171774429f1ef5b6a06ffa6b8e9ac15eb5a0e0627adbcbc146ea2203be619f217dce5351d7cb47e96a6e26b1092a94f1ba7a'
